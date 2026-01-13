6af4c8702b4fc7fefaaf0137c86378d76035d964
Asako Kanno <asa5ng13@gmail.com>
2026-01-13 21:58:22 +0100

    add icons

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export const App = () => {
 
       <ToDoCounter />
 
-      {/* <CounterComponent /> */}
+      <CounterComponent />
 
       <ToDoTasks />
 
--- src/components/ToDoCounter.jsx
+++ src/components/ToDoCounter.jsx
@@ -14,16 +14,16 @@ export const ToDoCounter = () => {
   )
 }
 
-// export const CounterComponent = () => {
-//   const { count, increment } = useStore()
+export const CounterComponent = () => {
+  const { count, increment } = useStore()
 
-//   return (
-//     <div>
-//       <p>Count: {count}</p>
-//       <button onClick={increment}>Increment</button>
-//     </div>
-//   )
-// }
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  )
+}
 
 const CounterContainer = styled.div`
   display: flex;
